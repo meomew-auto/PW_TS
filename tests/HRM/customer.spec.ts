35a9ae6601af86e7d440e3a039b577fb59e2d9b3
meomew_auto <hoangbui.ktmt@gmail.com>
2025-11-13 21:16:21 +0700

    chua bai thuc hanh 3

--- tests/HRM/customer.spec.ts
+++ tests/HRM/customer.spec.ts
@@ -1,9 +1,27 @@
 import { test, expect, Page } from '@playwright/test';
 import { format } from 'date-fns';
+import { faker } from '@faker-js/faker';
+
 const LOGIN_URL = 'https://crm.anhtester.com/admin/';
+
+function createRandomUser() {
+  return {
+    phone: faker.phone.number(),
+    vatNumber: faker.string.numeric(10),
+    website: faker.internet.url(),
+    currency: 'USD',
+    language: 'Vietnamese',
+    address: faker.location.streetAddress(),
+    city: faker.location.city(),
+    state: faker.location.state(),
+    zipcode: faker.location.zipCode(),
+    country: 'Vietnam',
+  };
+}
 
 async function loginAndNaviagteToNewCustomer(page: Page, tabName: string) {
   //thực hiện hành động login -> navigate tới customer
+
   //1. đầu tiên goto Page
   await page.goto(LOGIN_URL);
   //2. assert page có text Login
@@ -69,6 +87,111 @@ test.describe('CRM Customer Page - Possitive case', () => {
     // => lay 3118
     // await expect(page).toHaveURL(/clients\/client/);
   });
+
+  test('TC_CUST_02- Tạo Customer (Nhập đầy đủ thông tin)', async ({ page }) => {
+    //action dang nhap thanh cong
+    await loginAndNaviagteToNewCustomer(page, 'Customer');
+    // thu hep khoangh cah tim kiem -> dau tien la tim thang cha parent
+    // -> duyng locator chain de tim trong thang cha
+    const containerCompany = page.locator('label', { hasText: 'Company' });
+    // const containerCompany = page.locator('label').filter({ hasText: 'Company' });
+    const asterik = containerCompany.locator('small', { hasText: '*' });
+    await expect(asterik).toBeVisible();
+
+    const now = new Date();
+    const parsedDate = format(now, 'HH:mm:ss');
+    const companyName = `Auto PW company ${parsedDate}`;
+    await page.locator('#company').fill(companyName);
+
+    const information = createRandomUser();
+
+    //cac input của các ô
+    //VAT: #vat //input[@id='vat'] page.getByRole('textbox', { name: 'VAT Number' })
+    await page.getByRole('textbox', { name: 'VAT Number' }).fill(information.vatNumber);
+
+    //buoc tim ra dropdown va click mo
+    const currencyContainer = page.locator('div.form-group', { hasText: 'Currency' });
+    await currencyContainer.locator('button[data-id="default_currency"]').click();
+
+    await page
+      .locator('a[role="option"]')
+      .filter({ has: page.locator('span.text', { hasText: information.currency }) })
+      .click();
+
+    await page.locator('#default_language').selectOption(information.language);
+
+    //a[@role='option'][.//span[contains(@class, 'text') and contains(., 'USD') ]]
+
+    // await page.locator('#default_currency').selectOption(information.currency);
+
+    //xpath
+    // await page.locator("//button[@data-id='default_currency']").click();
+
+    //a[@role='option'][.//span[contains(@class, 'text') and contains(., 'USD') ]]
+    // a[id='bs-select-2-1']
+    // 0 1 2
+    // (#bs-select-2 a).nth(1)
+
+    await page
+      .locator('#profile-save-section')
+      .filter({ hasText: 'Save' })
+      .locator('button', { hasText: 'Save' })
+      .nth(1)
+      .click();
+
+    // await page.getByRole('button', { name: 'Save', exact: true }).click()
+    // div[@id='profile-save-section']//button[contains(normalize-space(.), "Save")].nth(1)
+    // (#profile-save-section button).nth(1)
+
+    await expect(page.locator('#alert_float_1')).toContainText('Customer added successfully.');
+    const currentUrl = page.url();
+    const urlParst = currentUrl.split('/clients/client/');
+    const customerId = urlParst[1];
+
+    const customerNameDisplay = page.locator('span.tw-truncate');
+    const displayedText = await customerNameDisplay.textContent();
+    expect(displayedText).toContain(customerId);
+
+    await expect(page.locator('#vat')).toHaveValue(information.vatNumber);
+    const dropdownText = await page
+      .locator('button[data-id="default_currency"]')
+      .getAttribute('title');
+
+    expect(dropdownText).toContain(information.currency);
+    const resultCurrencyDropdown = page.locator('div.form-group', { hasText: 'Currency' });
+    const itemCurrency = resultCurrencyDropdown.locator('//button[@data-id="default_currency"]');
+    const itemCurrentText = await itemCurrency.textContent();
+    console.log(itemCurrentText);
+  });
+
+  test('TC_CUST_04- Tạo Customer (Nhập đầy đủ thông tin)', async ({ page }) => {
+    //action dang nhap thanh cong
+    await loginAndNaviagteToNewCustomer(page, 'Customer');
+    // thu hep khoangh cah tim kiem -> dau tien la tim thang cha parent
+    // -> duyng locator chain de tim trong thang cha
+    const containerCompany = page.locator('label', { hasText: 'Company' });
+    // const containerCompany = page.locator('label').filter({ hasText: 'Company' });
+    const asterik = containerCompany.locator('small', { hasText: '*' });
+    await expect(asterik).toBeVisible();
+
+    const now = new Date();
+    const parsedDate = format(now, 'HH:mm:ss');
+    const companyName = `Auto PW company ${parsedDate}`;
+    await page.locator('#company').fill(companyName);
+
+    const information = createRandomUser();
+
+    //cac input của các ô
+    //VAT: #vat //input[@id='vat'] page.getByRole('textbox', { name: 'VAT Number' })
+    await page.getByRole('textbox', { name: 'VAT Number' }).fill(information.vatNumber);
+    await page.locator('#address').fill(information.address);
+
+    await page.getByRole('tab', { name: 'Billing & Shipping' }).click();
+
+    await page.getByText('Same as Customer Info').click();
+
+    await expect(page.locator('#billing_street')).toHaveValue(information.address);
+  });
 });
 
 //     test('TC_CUST_01', async ({ page }) => {
@@ -107,3 +230,4 @@ test.describe('CRM Customer Page - Possitive case', () => {
 //label[contains(normalize-space(.), "* Company"]/small[contains(normalize-space(.), "*")]
 //label[@for='company']/small[contains(normalize-space(.), "*")]
 //div[@id='profile-save-section']//button[contains(normalize-space(.), "Save")]
+//label[normalize-space(.) = "* Company"]//small
