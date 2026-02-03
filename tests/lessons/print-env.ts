5160f47e5734b0ffb73eb32bc1b0e679ddd50861
meomew_auto <hoangbui.ktmt@gmail.com>
2026-02-03 20:29:40 +0700

    print env

--- tests/lessons/print-env.ts
+++ tests/lessons/print-env.ts
@@ -1,22 +1,16 @@
 /**
  * Demo: In các biến môi trường từ file .env và GitHub Secrets/Vars
  * Sử dụng EnvManager có sẵn
- * 
+ *
  * Chạy local: npx tsx tests/lessons/print-env.ts
  * Chạy CI: Workflow sẽ truyền secrets/vars vào env
  */
 
-import { EnvManager } from '../utils/EnvManager.ts';
+import { EnvManager } from '../utils/EnvManager.js';
 
 console.log('==========================================');
 console.log('🔐 CÁC BIẾN MÔI TRƯỜNG');
 console.log('==========================================');
-console.log('');
-
-// Biến từ file .env (local)
-console.log('📁 TỪ FILE .ENV (LOCAL):');
-console.log('   PROJECT_NAME:', EnvManager.get('PROJECT_NAME', 'N/A'));
-console.log('   DEFAULT_EXPECT_TIMEOUT:', EnvManager.getNumber('DEFAULT_EXPECT_TIMEOUT', 0));
 console.log('');
 
 // Biến từ GitHub Secrets/Variables (CI)
@@ -27,7 +21,3 @@ console.log('');
 console.log('🌐 TỪ GITHUB VARIABLES:');
 console.log('   MY_WEBSITE:', EnvManager.get('MY_WEBSITE', '(chưa set - chỉ có trên CI)'));
 console.log('');
-
-console.log('==========================================');
-console.log('✅ Hoàn tất!');
-console.log('==========================================');
